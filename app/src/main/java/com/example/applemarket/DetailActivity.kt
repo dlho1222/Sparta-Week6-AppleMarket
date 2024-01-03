0380214aee1eccfc0945878de00e79bfb774fc11
ho lee <dlho1222@naver.com>
2024-01-03 14:16:29 +0900

    사과마켓 과제시작

---
+++ app/src/main/java/com/example/applemarket/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.applemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.applemarket.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
